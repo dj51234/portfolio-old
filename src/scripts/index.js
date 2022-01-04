796fe9dde3091a23cc37e8a92459f831a03cae5c
dj51234 <dj51234@yahoo.com>
2022-01-04 02:16:00 -0500

    removing experience section for now, need to do responsiveness for sm-md phone width

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -80,92 +80,92 @@ mobileNavItems.forEach((item) => {
 //   },
 // };
 
-const experienceContent = {
-  1: {
-    title: ['Data Systems Administrator', 'US Marine Corps'],
-    date: 'January 2017 - August 2021',
-    lineItems: [
-      'Mentored peers on fundamentals of HTML, CSS, and JavaScript and provided resources for assisted training via Marine Corps training app.',
-      'Worked in Active Directory creating user accounts, email accounts and troubleshooting common user errors within a virtual environment.',
-      'Served as ISSO for unit, a senior billet normally reserved for individuals 4 ranks higher than I was.',
-    ],
-    link: 'http://dj51234.github.io/CloudMe/',
-  },
-  2: {
-    title: ['Front End Developer', 'Red Ventures'],
-    date: 'August 2016 - January 2017',
-    lineItems: [
-      'Developed several versions of web pages with different functionality, components or advertisement text to leverage split testing.',
-      'Developed functional prototypes for clients to ensure quality control before the project development  phase.',
-      'Tested front-end code in multiple browsers to ensure cross-browser compatibility.',
-    ],
-    link: 'https://card.americanexpress.com/d/delta-reserve-credit-card/',
-  },
-  3: {
-    title: ['Creative Developer', 'Investor Management Services'],
-    date: 'December 2015 - August 2016',
-    lineItems: [
-      'Developed and managed website for a financial company using HTML5, CSS3, jQuery and Wordpress',
-      'Oversaw all design and development phases of website development for client websites.',
-      'Worked in an agile/waterfall/JIRA structure and created HTML email templates and newsletters for email marketing campaigns.',
-    ],
-    link: 'https://www.investormanagementservices.com/',
-  },
-  4: {
-    title: ['Front End Developer', 'Linium'],
-    date: 'September 2014 - December 2015',
-    lineItems: [
-      'Developed and designed client websites using Photoshop, HTML5 and CSS.',
-      'Designed wireframes and functional web prototypes using front end development tools.',
-      'Maintained the website by running tests, consistently updating features and fixing bugs.',
-    ],
-    link: 'http://dj51234.github.io/CloudMe/',
-  },
-};
+// const experienceContent = {
+//   1: {
+//     title: ['Data Systems Administrator', 'US Marine Corps'],
+//     date: 'January 2017 - August 2021',
+//     lineItems: [
+//       'Mentored peers on fundamentals of HTML, CSS, and JavaScript and provided resources for assisted training via Marine Corps training app.',
+//       'Worked in Active Directory creating user accounts, email accounts and troubleshooting common user errors within a virtual environment.',
+//       'Served as ISSO for unit, a senior billet normally reserved for individuals 4 ranks higher than I was.',
+//     ],
+//     link: 'http://dj51234.github.io/CloudMe/',
+//   },
+//   2: {
+//     title: ['Front End Developer', 'Red Ventures'],
+//     date: 'August 2016 - January 2017',
+//     lineItems: [
+//       'Developed several versions of web pages with different functionality, components or advertisement text to leverage split testing.',
+//       'Developed functional prototypes for clients to ensure quality control before the project development  phase.',
+//       'Tested front-end code in multiple browsers to ensure cross-browser compatibility.',
+//     ],
+//     link: 'https://card.americanexpress.com/d/delta-reserve-credit-card/',
+//   },
+//   3: {
+//     title: ['Creative Developer', 'Investor Management Services'],
+//     date: 'December 2015 - August 2016',
+//     lineItems: [
+//       'Developed and managed website for a financial company using HTML5, CSS3, jQuery and Wordpress',
+//       'Oversaw all design and development phases of website development for client websites.',
+//       'Worked in an agile/waterfall/JIRA structure and created HTML email templates and newsletters for email marketing campaigns.',
+//     ],
+//     link: 'https://www.investormanagementservices.com/',
+//   },
+//   4: {
+//     title: ['Front End Developer', 'Linium'],
+//     date: 'September 2014 - December 2015',
+//     lineItems: [
+//       'Developed and designed client websites using Photoshop, HTML5 and CSS.',
+//       'Designed wireframes and functional web prototypes using front end development tools.',
+//       'Maintained the website by running tests, consistently updating features and fixing bugs.',
+//     ],
+//     link: 'http://dj51234.github.io/CloudMe/',
+//   },
+// };
 
-function loadPortfolioContent() {
-  const thumbs = document.querySelectorAll('.thumb-overlay');
+// function loadPortfolioContent() {
+//   const thumbs = document.querySelectorAll('.thumb-overlay');
 
-  thumbs.forEach((thumb) => {
-    const currentNum = thumb.dataset.content;
-    thumb.parentElement.style.background = `url('src/images/content_img_${currentNum}.jpg')`;
-    thumb.parentElement.style.backgroundSize = 'contain';
-    thumb.children[0].textContent = portfolioContent[currentNum].title[0];
-    thumb.children[0][0].textContent = portfolioContent[currentNum].title[1];
-    thumb.children[1].textContent = portfolioContent[currentNum].description;
-    thumb.children[2].textContent = portfolioContent[currentNum].tools;
-  });
-}
+//   thumbs.forEach((thumb) => {
+//     const currentNum = thumb.dataset.content;
+//     thumb.parentElement.style.background = `url('src/images/content_img_${currentNum}.jpg')`;
+//     thumb.parentElement.style.backgroundSize = 'contain';
+//     thumb.children[0].textContent = portfolioContent[currentNum].title[0];
+//     thumb.children[0][0].textContent = portfolioContent[currentNum].title[1];
+//     thumb.children[1].textContent = portfolioContent[currentNum].description;
+//     thumb.children[2].textContent = portfolioContent[currentNum].tools;
+//   });
+// }
 
-function loadExperienceContent() {
-  const experienceItems = document.querySelectorAll('.experience-list a');
-  const experience = document.querySelector('.experience-content').children;
-  const experienceParas = document.querySelectorAll('.experience-item');
-  experienceItems.forEach((item) => {
-    item.addEventListener('click', function (e) {
-      clearExperienceNav();
-      e.preventDefault();
-      const currentNum = item.dataset.experience;
-      let counter = 0;
-      item.classList.contains('active')
-        ? item.classList.remove('active')
-        : item.classList.add('active');
-      experience[0].innerHTML = `<span>${experienceContent[currentNum].title[0]}</span> @ ${experienceContent[currentNum].title[1]}`;
-      experience[1].textContent = experienceContent[currentNum].date;
-      experienceParas.forEach((para) => {
-        para.textContent = experienceContent[currentNum].lineItems[counter];
-        counter++;
-      });
-    });
-  });
-}
+// function loadExperienceContent() {
+//   const experienceItems = document.querySelectorAll('.experience-list a');
+//   const experience = document.querySelector('.experience-content').children;
+//   const experienceParas = document.querySelectorAll('.experience-item');
+//   experienceItems.forEach((item) => {
+//     item.addEventListener('click', function (e) {
+//       clearExperienceNav();
+//       e.preventDefault();
+//       const currentNum = item.dataset.experience;
+//       let counter = 0;
+//       item.classList.contains('active')
+//         ? item.classList.remove('active')
+//         : item.classList.add('active');
+//       experience[0].innerHTML = `<span>${experienceContent[currentNum].title[0]}</span> @ ${experienceContent[currentNum].title[1]}`;
+//       experience[1].textContent = experienceContent[currentNum].date;
+//       experienceParas.forEach((para) => {
+//         para.textContent = experienceContent[currentNum].lineItems[counter];
+//         counter++;
+//       });
+//     });
+//   });
+// }
 
-function clearExperienceNav() {
-  const experienceItems = document.querySelectorAll('.experience-list a');
-  experienceItems.forEach((link) => {
-    link.classList.remove('active');
-  });
-}
+// function clearExperienceNav() {
+//   const experienceItems = document.querySelectorAll('.experience-list a');
+//   experienceItems.forEach((link) => {
+//     link.classList.remove('active');
+//   });
+// }
 
-// loadPortfolioContent();
-loadExperienceContent();
+// // loadPortfolioContent();
+// loadExperienceContent();
